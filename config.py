0dec05d85f69f702663682beb9987345d46bad60
probnakka-sys <probnakka@gmail.com>
2026-03-15 18:56:03 +0600

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,12 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+if not TELEGRAM_TOKEN:
+    TELEGRAM_TOKEN = "8472133401:AAEYcS6g7k7Hd50I2hrxlq5jX4p9uOXkk5s"
+    print("⚠️ Использую захардкоженный токен для теста")
+print(f"🚀 DEBUG - TELEGRAM_TOKEN из os.getenv: {os.getenv('TELEGRAM_TOKEN')}")
+print(f"🚀 DEBUG - TELEGRAM_TOKEN из os.environ: {os.environ.get('TELEGRAM_TOKEN')}")
+print(f"🚀 DEBUG - Все переменные: {list(os.environ.keys())}")
 
 # ============== НАСТРОЙКИ БОТА ==============
 
